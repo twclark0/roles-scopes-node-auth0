3a523c924b5a5bf881d42ddb1d7c1a9255553200
twclark0 <tylerc644@gmail.com>
2020-09-23 08:48:20 -0600

    Fix insufficent scope error

--- api-server.js
+++ api-server.js
@@ -50,7 +50,7 @@ app.get("/api/private", checkJwt, (req, res) => {
   });
 });
 
-app.get("/api/private/role", checkScopes, checkJwt, (req, res) => {
+app.get("/api/private/role", checkJwt, checkScopes, (req, res) => {
   res.send({
     msg: "Your access token was successfully validated!"
   });
